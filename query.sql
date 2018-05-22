40e07a2523e6d97ed839c5bd93d23c0848028876
kevinheavey <heaveykevin2@gmail.com>
2018-05-22 22:49:46 +0100

    more parsing of expr

--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
-select m.col1, m.col2 / 2 as newname, col3, m.col4 + col5 as x, 1 as y, col7 as z from mytable m;
+select m.col1, m.col2 / 2 as newname, col3, m.col4 + col5 as x, 1 as y, col7 as z, *, m.* from mytable m;
 select foo from baz;
